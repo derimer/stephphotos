96d87685cc06a36e465d0038bceff4d8cc8f0780
derimer <jderimer@gmail.com>
2024-12-11 17:47:27 +0100

    vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: "./",
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: "dist", // Répertoire de sortie pour la production
   },
 });
