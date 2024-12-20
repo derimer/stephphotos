08f9880a3cba3db08a525fb1b34da3bda9771dc0
derimer <jderimer@gmail.com>
2024-12-20 08:36:45 +0100

    ajustement root

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,14 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Accueil from "./components/Accueil";
 
 function App() {
   return (
     <>
       <NavBar />
-
+      <Accueil />
       <Outlet />
-
       <Footer />
     </>
   );
