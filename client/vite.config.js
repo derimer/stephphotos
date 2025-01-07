3ae0184d2af8a8c58ba79af6f57f62f402c80f99
derimer <jderimer@gmail.com>
2025-01-07 17:20:51 +0100

    modif vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   root: ".", // Définit la racine du projet
   build: {
@@ -25,4 +25,5 @@ export default defineConfig({
       },
     },
   },
-});
+  base: mode === "production" ? "./" : "/", // Définit le chemin de base pour la production
+}));
