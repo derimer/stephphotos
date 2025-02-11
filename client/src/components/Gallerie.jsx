5f42163e51d8354ab5fd82fb7a58b4645b6d34f4
derimer <jderimer@gmail.com>
2025-02-11 07:38:46 +0100

    refonte TITRE galleries

--- client/src/components/Gallerie.jsx
+++ client/src/components/Gallerie.jsx
@@ -16,63 +16,61 @@ export default function Gallerie() {
       <main id="gallerie1">
         <h1>Galeries</h1>
         <ul id="galeries">
-          <li>
+          <li className="image-container">
             <Link to="/gallerie/singleGallery?id=1">
               <img src={Gerbier} alt="mont gerbier" />
               <h2>Voyages n&b</h2>
             </Link>
           </li>
-          <li>
+          <li className="image-container">
             <Link to="/gallerie/singleGallery?id=2">
               <img src={Hotel} alt="" />
               <h2>Voyages couleur</h2>
             </Link>
           </li>
-          <li>
+          <li className="image-container">
             <Link to="/gallerie/singleGallery?id=3">
               <img src={Pecheurs} alt="pecheurs" />
               <h2> sublime N&B</h2>
             </Link>
           </li>
-          <li>
+          <li className="image-container">
             <Link to="/gallerie/singleGallery?id=4">
               <img src={Bouteilles} alt="bouteilles" />
               <h2>sublime Couleur</h2>
             </Link>
           </li>
-          <li>
+          <li className="image-container">
             <Link to="/gallerie/singleGallery?id=5">
               <img src={Acteur} alt="Acteur" />
               <h2>Portrait n&b</h2>
             </Link>
           </li>
-          <li>
+          <li className="image-container">
             <Link to="/gallerie/singleGallery?id=6">
               <img src={Audray} alt="Audray" />
               <h2>Portrait couleur</h2>
             </Link>
           </li>
-          <li>
+          <li className="image-container">
             <Link to="/gallerie/singleGallery?id=7">
               <img src={Lilly} alt="Lilly" />
               <h2>Charme n&b</h2>
             </Link>
           </li>
-          
-          <li>
+          <li className="image-container">
             <Link to="/gallerie/singleGallery?id=8">
               <img src={Julie} alt="Julie" />
               <h2>Charme Couleur</h2>
             </Link>
           </li>
-
-          <li>
+          <li className="image-container">
             <Link to="/gallerie/singleGallery?id=9">
               <img src={Lyon} alt="Lyon" />
               <h2>Evenements n&b</h2>
             </Link>
           </li>
-          <li>
+          <li className="image-container">
             <Link to="/gallerie/singleGallery?id=10">
               <img src={Rmc} alt="rmc" />
               <h2>Evenements Couleur</h2>
